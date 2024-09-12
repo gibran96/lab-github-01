1f47c46ee866d62a54369fcaaae65c39b4eeef13
Gibran <myageri.g@northeastern.edu>
2024-09-12 18:47:22 -0400

    testing actions

--- test/test_pytest.py
+++ test/test_pytest.py
@@ -1,3 +1,12 @@
+import sys
+import os
+import unittest
+
+# Get the path to the project's root directory
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_root)
+
+
 import pytest
 from src import calculator
 
@@ -6,6 +15,7 @@ def test_fun1():
     assert calculator.fun1(5,0) == 5
     assert calculator.fun1 (-1, 1) == 0
     assert calculator.fun1 (-1, -1) == -2
+    print('test_fun1 passed')
 
 
 def test_fun2():
@@ -14,6 +24,8 @@ def test_fun2():
     assert calculator.fun2 (-1, 1) == -2
     assert calculator.fun2 (-1, -1) == 0
 
+    print('test_fun2 passed')
+
 def test_fun3():
     assert calculator.fun3(2, 3) == 6
     assert calculator.fun3(5,0) == 0
@@ -21,12 +33,16 @@ def test_fun3():
     
     assert calculator.fun3 (-1, -1) == 1
 
+    print('test_fun3 passed')
+
 def test_fun4():
     assert calculator.fun4(2, 3, 5) == 10
     assert calculator.fun4(5,0, -1) == 4
     assert calculator.fun4 (-1, -1, -1) == -3
     
     assert calculator.fun4 (-1, -1, 100) == 98
+
+    print('test_fun4 passed')
     
 # # Parametrized Tests
 # @pytest.mark.parametrize("a, b, expected", [(2, 3, 5), (5, 0, 5), (-1, 1, 0), (-1, -1, -2)])
